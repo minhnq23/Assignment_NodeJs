e1c9f170b66ce6e2cfeca34dd91312ba9b370db9
admin <minhnqph25450@fpt.edu.vn>
2023-03-22 19:46:15 +0700

    mes

--- router.js
+++ router.js
@@ -6,3 +6,8 @@ router.get("/register", function (req, res, next) {
 });
 
 module.exports = router;
+
+/// dùng thư viện express tạo router  ở dòng số 2 rồi dùng lệnh get ddeer render trang register
+// export router ra để dùng muốn dùng cái gì ở file khác thì phải export
+// ở trang app.js gọi và sử dụng lệnh user để dùng
+// sử lý sự kiên thẻ a thì herf = "/register" còn cái khác thì dùng js để sử lý
